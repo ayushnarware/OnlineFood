d7cc148934d30a9fc0c4b97a1ec3cbe71265232c
Ayush Narware <152053797+ayushnarware@users.noreply.github.com>
2025-04-15 00:21:52 +0530

    initial commit

--- src/Routers/CustomerRoutes.jsx
+++ src/Routers/CustomerRoutes.jsx
@@ -13,6 +13,7 @@ import NotFound from '../customers/pages/NotFound/NotFound'
 
 
 
+
 const CustomerRoutes = () => {
   return (
     <div className='relative'>
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store } from './State/Store/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/OnlineFood">
       <Provider store={store}>
         <App />
       </Provider> 
